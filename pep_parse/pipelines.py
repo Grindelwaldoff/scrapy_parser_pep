76649ad8622f12daaf127188e18eb614a89f3b80
Grindelwaldoff <vsevolodrybnik@gmail.com>
2023-09-09 13:28:13 +0300

    review

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,41 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import datetime
+import os
+import csv
+from collections import defaultdict
+
+from pep_parse.settings import TIME_FORMAT, SUMMARY_FILENAME, RESULT_DIRNAME, BASE_DIR
+
+
+class PepParsePipeline:
+    def open_spider(self, spider):
+        self.statuses = defaultdict(int)
+
+    def process_item(self, item, spider):
+        self.statuses[item['status']] += 1
+        print(item['status'])
+        return item
+
+    def close_spider(self, spider):
+        time = datetime.datetime.now().strftime(TIME_FORMAT)
+        with open(
+            f'{BASE_DIR / RESULT_DIRNAME}/{SUMMARY_FILENAME.format(time=time)}',
+            mode='w',
+            encoding='utf-8'
+        ) as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=['Status', 'Amount'])
+            writer.writeheader()
+            for key, value in self.statuses.items():
+                writer.writerow({'Status': key, 'Amount': value})
+            writer.writerow(
+                {
+                    'Status': 'Total',
+                    'Amount': sum(list(self.statuses.values()))
+                }
+            )
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,45 @@
+from urllib.parse import urljoin
+
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response):
+        index_by_category_section = response.css(
+            'section#index-by_category'
+        )
+        numerical_index_section = response.css(
+            'section#numerical-index'
+        )
+        pep_rows = (
+            index_by_category_section.css('td')
+            + numerical_index_section.css('td')
+        )
+        for pep_row_index in range(0, len(pep_rows), 4):
+            # table_status = pep_rows[pep_row_index].css('abbr::text').get()
+            pep_link = urljoin(
+                response._url,
+                pep_rows[pep_row_index + 1].css('a::attr(href)').extract()[0]
+            )
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        title = response.css('h1.page-title::text').get().split()
+        for dt in response.xpath('//dl/dt'):
+            if dt.css('::text').get() == 'Status':
+                status = dt.xpath(
+                    'string(following-sibling::dd[1])'
+                ).extract()[0]
+        yield PepParseItem(
+            {
+                'name': ' '.join(title[3:]),
+                'number': title[1],
+                'status': status,
+            }
+        )
